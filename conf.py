80af02c2023b44d3e6f87f29913831461dd29f7e
Denno Louis <54968137+dennolouis@users.noreply.github.com>
2022-08-04 16:00:10 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'Denno'
+copyright = '2022, Denno Louis'
+author = 'Denno Louis'
 
 
 
